b5da189e230b1e229a1007e0b23fabac4cad2a63
unknown <z.matinfa77@gmail.com>
2024-09-18 01:41:47 +0330

    Register custom user in admin panel

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,3 +1,54 @@
+from typing import Any
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+User = get_user_model()
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    add_form_template = "admin/auth/user/add_form.html"
+    change_user_password_template = None
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "usable_password", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("first_name", "last_name", "email")
+    ordering = ("-date_joined", "email", )
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        qs = super().get_queryset(request)
+        return qs.filter(is_staff=False)
+
